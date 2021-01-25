5af2049420b619046724b5096d634cdcab5396ae
Ronlin1 <ronlinx6@gmail.com>
2021-01-25 13:38:20 +0300

    updated code snippet

--- card-shuffler.py
+++ card-shuffler.py
@@ -4,12 +4,13 @@
 import itertools, random
 
 # make a deck of cards
-deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
+deck = list(itertools.product(range(1, 14), ['Spade', 'Heart', 'Diamond', 'Club']))
 
 # shuffle the cards with random
 random.shuffle(deck)
 
 # draw five cards randomly
-print("You ve'got: ")
+print("\nYay you got: \n")
 for i in range(5):
-   print(deck[i][0], "of", deck[i][1])
+    print('------------>', deck[i][0], "of", deck[i][1])
+print('\nRun to re-shuffle again --->>>>>>')
